e178ae2954943b8d7ee94c25042883fe29979fc0
miraxsd <60142429+miraxsd@users.noreply.github.com>
2020-11-24 11:50:58 -0500

    V1
    Co-Authored-By: sasafc <60142097+sasafc@users.noreply.github.com>
    Co-Authored-By: OmarSadat6 <58267128+OmarSadat6@users.noreply.github.com>

---
+++ TP2_LOG2810/TP2_LOG2810/Tree.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <map>
+#include <set>
+#include <iostream>
+#include <fstream>
+#include <sstream>
---
+++ TP2_LOG2810/TP2_LOG2810/Tree.h
@@ -0,0 +1,107 @@
+#pragma once
+#ifndef TREE_H
+#define TREE_H 
+
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+template <typename ValueType>
+/*static private */class Node {
+private:
+    ValueType value;
+    bool isLastChar = false;
+    Node<ValueType> parent; // Pointer to the node containing this node
+    Vector<Node* <ValueType>> children = null; // Pointer to the node on the left which should contain a value below this.value
+
+    int height = 0;
+
+    Node* getChild(std::string value) {
+        for (ValueType childValue : children) {
+            if (childValue == value.r)
+
+
+        }
+    }
+
+public:
+    Node(ValueType value, Node<ValueType> parent, bool isLastChar)
+    {
+        this.value = value;
+        this.parent = parent;
+        this.isLastChar = isLastChar;
+    }
+    ValueType getValue{ return value; }
+};
+
+template <typename ValueType>
+class AvlTree {
+
+    // Only node which has its parent to null
+private:
+
+    Node* <ValueType> root;
+
+public:
+
+    AvlTree() { }
+    public void add(ValueType value) {
+        Boolean insertionSuccess = false;
+        if (root == null)
+        {
+            root = Node<>(value, null);
+            return;
+        }
+    }
+    // Prend le noeud de départ et la valeur qu'il cherche.
+    // Cherche la valeur dans les liste des enfants noeuds 
+    // S'il le trouve il retourne le noeud qui contient la valeur
+    // Sinon il retourne le noeud ou on doit ajouter la valeur dans la liste de ses enfants
+    public Node* getChild(Node* node, std::string value) {
+        Node* finalChild = null;
+        for (Node<ValueType>* child : children) {
+            if (child->getValue() == value.substr(0, child->getValue().length()))
+                finalChild=getChild(child, child->getValue().length() + 1);
+        }
+        return finalChild;
+    }
+};
+
+#endif
+
+ /*
+    if (contains(value))
+        return;
+    BinaryNode<ValueType> node = root;
+    while ((node != null) && (node.value != value))
+    {
+        int compareResult = value.compareTo(node.value);
+        if (compareResult < 0) {
+            if ((node.left != null))
+                node = node.left;
+            else {
+                node.left = new BinaryNode<>(value, node);
+                //node.height = Math.max(node.left.height, node.right.height)+1;
+                insertionSuccess = true;
+
+                break;
+            }
+        }
+        else if (compareResult > 0) {
+            if ((node.right != null))
+                node = node.right;
+            else {
+                node.right = new BinaryNode<>(value, node);
+                //node.height = Math.max(node.left.height, node.right.height)+1;
+                insertionSuccess = true;
+                break;
+            }
+        }
+    }
+    if (insertionSuccess) {
+        AvlTree tree = new AvlTree<ValueType>();
+        tree.root = node;
+        node.height = tree.getHeight();
+        balance(root);
+}*/
